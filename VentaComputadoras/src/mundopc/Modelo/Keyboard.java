290a9d24ac10e1e24fdd81bf4f92eb05cd52d3ba
Ed Pad <ed.padr@gmail.com>
2025-03-19 14:06:39 -0600

    add class Keyboard

---
+++ VentaComputadoras/src/mundopc/Modelo/Keyboard.java
@@ -0,0 +1,23 @@
+package mundopc.Modelo;
+
+// Inheritance
+class Keyboard extends InputDevice{
+    private final int idKeyboard;
+    private static int Keyboard_counter;
+    
+    //Constructor applies inheritance by calling the parent class
+    public Keyboard(String device, String brand){
+        super(device, brand);
+        idKeyboard=++Keyboard_counter;
+    }
+    
+    
+    @Override
+    public String toString(){
+        return "Keyboard [" +
+            "idKeyboard: " + idKeyboard +
+            "]" + super.toString();
+    }
+    
+}
+
