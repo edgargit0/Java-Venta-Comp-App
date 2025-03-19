381898fc3b9e43f78f222bc155e06799f8cede0c
Ed Pad <ed.padr@gmail.com>
2025-03-19 14:15:15 -0600

    add class Model/Monitor

--- VentaComputadoras/src/mundopc/Modelo/Monitor.java
+++ VentaComputadoras/src/mundopc/Modelo/Monitor.java
@@ -1,5 +1,51 @@
 package mundopc.Modelo;
 
-public class Monitor {
 
+class Monitor{
+    private final int idMonitor;
+    private String brand;
+    private double size;
+    private static int monitor_counter;
+    
+    //Constructors
+    private Monitor(){
+        idMonitor = ++monitor_counter;
+    }
+    
+    public Monitor(String brand, double size){
+        this();
+        this.brand=brand;
+        this.size=size;
+    }
+    
+    //getters-setters
+    public int getIdMonitor(){
+        return idMonitor;
+    }
+    
+    public String getBrand(){
+        return brand;
+    }
+    
+    public void setBrand(String brand){
+        this.brand=brand;
+    }
+    
+    public double getSize(){
+        return size;
+    }
+    
+    public void setSize(double size){
+        this.size=size;
+    }
+    
+    @Override
+    public String toString(){
+        return "Monitor [" +
+            "idMonitor: " + idMonitor + ", " +
+            "brand: " + brand + ", " +
+            "size: " + size +
+            "]";
+    }
+    
 }
