2398f9c76e642cf8c2d8d2f63ecfdde3779665c9
Ed Pad <ed.padr@gmail.com>
2025-03-19 13:58:02 -0600

    add code to class InputDevice

--- VentaComputadoras/src/mundopc/Modelo/InputDevice.java
+++ VentaComputadoras/src/mundopc/Modelo/InputDevice.java
@@ -1,5 +1,38 @@
 package mundopc.Modelo;
 
-public class InputDevice {
-
+// Computer Sales App
+class InputDevice{
+    private String device;
+    private String brand;
+    
+    public InputDevice(String device, String brand){
+        this.device=device;
+        this.brand=brand;
+    }
+    
+    //getters-setters
+    public String getDevice(){
+        return device;
+    }
+    
+    public void setDevice(String device){
+        this.device=device;
+    }
+    
+    public String getBrand(){
+        return brand;
+    }
+    
+    public void setBrand(String brand){
+        this.brand=brand;
+    }
+    
+    @Override
+    public String toString(){
+        return "InputDevice [" + 
+            "device: " + device + ", " +
+            "brand: " + brand +
+            "]";
+    }
+    
 }
