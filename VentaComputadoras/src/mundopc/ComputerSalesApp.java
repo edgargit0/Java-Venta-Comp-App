aa71e6f59aa6445a2db8765ee2dcce9eb39a5a2d
Ed Pad <ed.padr@gmail.com>
2025-03-19 15:02:35 -0600

    add code to main file Computer Sales App

--- VentaComputadoras/src/mundopc/ComputerSalesApp.java
+++ VentaComputadoras/src/mundopc/ComputerSalesApp.java
@@ -8,6 +8,44 @@ import mundopc.service.Sale;
 
 public class ComputerSalesApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Crear objetos
+    Mouse mouseLenovo = new Mouse("bluetooth", "Lenovo");
+    //System.out.println(MouseLenovo);
+    Keyboard keyboardLenovo = new Keyboard("bluetooth", "Lenovo");
+    //System.out.println(KeyboardLenovo);
+    Monitor monitorLenovo = new Monitor("Lenovo", 27);
+    //System.out.println(monitorLenovo);
+    // Creamos un objeto de tipo Computer
+    Computer computerLenovo =
+            new Computer("Computer Lenovo", monitorLenovo,
+                    keyboardLenovo, mouseLenovo);
+    //System.out.println(ComputerLenovo);
+
+    // Objeto Computer
+    Monitor monitorDell = new Monitor("Dell", 15);
+    Keyboard keyboardDell = new Keyboard("usb", "Dell");
+    Mouse mouseDell = new Mouse("usb", "Dell");
+    Computer computerDell = new Computer("Computer Dell",
+            monitorDell, keyboardDell, mouseDell);
+
+    // Creamos una Sale
+    Sale Sale1 = new Sale();
+    Sale1.addComputer(computerLenovo);
+    Sale1.addComputer(computerDell);
+    Sale1.showSale();
+
+    // Computer Mac
+    Monitor monitorMac = new Monitor("Mac", 27);
+    Keyboard keyboardMac = new Keyboard("bluetooth", "Mac");
+    Mouse mouseMac = new Mouse("bluetooth", "Mac");
+    Computer computerIMac = new Computer("iMac",
+            monitorMac, keyboardMac, mouseMac);
+
+    Sale Sale2 = new Sale();
+    Sale2.addComputer(computerIMac);
+    Sale2.addComputer(computerDell);
+    Sale2.addComputer(computerLenovo);
+    System.out.println();
+    Sale2.showSale();
     }
 }
